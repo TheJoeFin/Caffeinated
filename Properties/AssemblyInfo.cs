4a88f2651cdcfa503c435d8e63b7425a62dbff9c
Joseph Finney <josephfinney@LIVE.COM>
2021-01-20 06:50:22 -0600

    Update AssemblyInfo to new version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Caffeinated")]
+[assembly: AssemblyTitle("Windows Caffeinated")]
 [assembly: AssemblyDescription("Prevents Windows from sleeping.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Desmond Brand")]
-[assembly: AssemblyProduct("Caffeinated")]
-[assembly: AssemblyCopyright("Copyright © Desmond Brand 2011")]
+[assembly: AssemblyCompany("JoeFinApps")]
+[assembly: AssemblyProduct("Windows Caffeinated")]
+[assembly: AssemblyCopyright("Copyright © JoeFinApps 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
