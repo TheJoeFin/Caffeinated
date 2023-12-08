ea4ba318cc158d634717fdba34a1dd56c60a42ee
Joseph Finney <TheJoeFin@users.noreply.github.com>
2023-12-08 16:10:42 -0600

    first pass at enabling trimming

--- Helpers/NativeMethods.cs
+++ Helpers/NativeMethods.cs
@@ -2,9 +2,9 @@
 
 namespace Caffeinated;
 
-internal static class NativeMethods {
-    [DllImport("kernel32.dll")]
-    public static extern uint SetThreadExecutionState(uint esFlags);
+internal static partial class NativeMethods {
+    [LibraryImport("kernel32.dll")]
+    public static partial uint SetThreadExecutionState(uint esFlags);
     public const uint ES_CONTINUOUS = 0x80000000;
     public const uint ES_SYSTEM_REQUIRED = 0x00000001;
     public const uint ES_DISPLAY_REQUIRED = 0x00000002;
